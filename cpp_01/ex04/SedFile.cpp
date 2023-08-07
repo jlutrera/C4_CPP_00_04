6ed51de172a2b9b41964ae985f93e151ce813917
jutrera- <jl_utrera_canalejo@yahoo.es>
2023-08-07 13:13:08 +0200

    cambios en ex04 de cpp01

--- cpp_01/ex04/SedFile.cpp
+++ cpp_01/ex04/SedFile.cpp
@@ -6,7 +6,7 @@
 /*   By: jutrera- <jutrera-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 13:41:53 by jutrera-          #+#    #+#             */
-/*   Updated: 2023/08/01 14:36:06 by jutrera-         ###   ########.fr       */
+/*   Updated: 2023/08/07 13:11:14 by jutrera-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,29 +30,29 @@ void SedFile::ft_replace( void )
     std::string ptr;
 
 	_infile.open(_fin.c_str());
-	if (_infile.is_open())
+	if (!_infile.is_open())
 	{
-    	if (std::getline(_infile, s, '\0'))
-		{
-			_outfile.open(_fout.c_str());
-			if (_outfile.is_open())
-			{
-		        found = s.find(_s1);
-				ptr = "";
-		        while (found != std::string::npos)
-	    	    {    
-					ptr += s.substr( 0, found ) + _s2;
-					s.erase( 0, found + _s1.length() );
-	            	found = s.find( _s1 );
-	        	}
-	        	_outfile << ptr + s;
-	 		}
-			else
-				std::cerr << _fout + " cannot be created !" << std::endl;
-		}
-		else
-			std::cerr << _fin + " is an empty file !" << std::endl;
-	}
-	else
 		std::cerr << _fin + " cannot be opened !" << std::endl;
+		return;
+	}
+	if (!std::getline(_infile, s, '\0'))
+	{
+		std::cerr << _fin + " is an empty file !" << std::endl;
+		return;
+	}
+	_outfile.open(_fout.c_str());
+	if (!_outfile.is_open())
+	{
+		std::cerr << _fout + " cannot be created !" << std::endl;
+		return;
+	}
+	found = s.find(_s1);
+	ptr = "";
+	while (found != std::string::npos)
+		{    
+			ptr += s.substr( 0, found ) + _s2;
+			s.erase( 0, found + _s1.length() );
+	      	found = s.find( _s1 );
+	    }
+	_outfile << ptr + s;
 }
