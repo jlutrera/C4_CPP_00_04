de2c548f7ff4b8736d2ea3ed1ff4b901ff58f848
jutrera- <jutrera-@c1r12s2.42madrid.com>
2023-08-22 20:46:53 +0200

    ex03 no iba bien

--- cpp_02/ex03/bsp.cpp
+++ cpp_02/ex03/bsp.cpp
@@ -6,39 +6,22 @@
 /*   By: jutrera- <jutrera-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 20:21:11 by jutrera-          #+#    #+#             */
-/*   Updated: 2023/08/07 19:01:42 by jutrera-         ###   ########.fr       */
+/*   Updated: 2023/08/22 20:21:48 by jutrera-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
-/*
-Let the coordinates of three vertices A(x1, y1), B(x2, y2) and C(x3, y3). And coordinates of the given point P(x, y)
 
-Calculate area of the given triangle, i.e., area of the triangle ABC in the above diagram. 
-             Area A = [ x1(y2 – y3) + x2(y3 – y1) + x3(y1-y2) ] / 2 
-Calculate area of the triangle PAB. We can use the same formula for this. Let this area be A1. 
-Calculate area of the triangle PBC. Let this area be A2. 
-Calculate area of the triangle PAC. Let this area be A3. 
-
-If A1, A2 or A2 is zero, then P point is on the edge or coincides with one of the vertices. 
-And P is not inside the triangle !!!!
-
-If P lies inside the triangle, then A1 + A2 + A3 must be equal to A. 
-*/
 Fixed surface( Point const a, Point const b, Point const c )
 {
-	Fixed t1 = a.getX() * (b.getY() - c.getY()) / 2;
-	Fixed t2 = b.getX() * (a.getY() - c.getY()) / 2; 
-	Fixed t3 = c.getX() * (b.getY() - a.getY()) / 2;
+	Fixed t1 = ( b.getX() - a.getX() ) * ( c.getY() - a.getY()) / 2;
+	Fixed t2 = ( c.getX() - a.getX() ) * ( b.getY() - a.getY()) / 2; 
+	Fixed t3 = t1- t2;
 
-	if (t1 < 0)
-		t1 = t1 * (-1);
-	if (t2 < 0)
-		t2 = t2 * (-1);
 	if (t3 < 0)
 		t3 = t3 * (-1);
 	
-	return t1 + t2 + t3;
+	return t3;
 }
 
 bool bsp( Point const a, Point const b, Point const c, Point const p)
